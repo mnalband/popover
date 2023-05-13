4be918b27cb38c313dc3ca8037bfe7355c689f9f
Maryna Nalbandian <maringo30@gmail.com>
2023-05-13 16:25:41 +0200

    Update button.test.js

--- e2e/button.test.js
+++ e2e/button.test.js
@@ -6,7 +6,7 @@ describe("button's work", () => {
 
   beforeEach(async () => {
     browser = await puppeteer.launch({
-      headless: false,
+      headless: true,
       slowMo: 100,
       devtools: true,
     });
